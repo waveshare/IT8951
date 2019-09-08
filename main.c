f34635796888b8337a1043787e7364752e1938d3
charly <charly@zen.ly>
2019-09-08 23:02:32 +0200

    write direct to buffer

--- main.c
+++ main.c
@@ -89,11 +89,9 @@ int main (int argc, char *argv[])
 
         printf("read file start\n");
     uint8_t *buffer = read_png_file(argv[1], &width, &height, &color_type, &bit_depth, buffer_to_write);
-printf("read file end\n");
+    printf("read file end\n");
     printf("update screen start\n");
-    for (int i = 0; i < 10; ++i) {
-        IT8951_Display4BppBuffer(buffer);
-    }
+    IT8951_Display4BppBuffer(buffer);
     printf("update screen end\n");
 
 	
